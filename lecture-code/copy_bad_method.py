5500c68ea9df9db9735b884df4df6c07308cf8fe
TurtleInATophat <101486835+TurtleInATophat@users.noreply.github.com>
2022-03-13 14:45:23 +1100

    nikkis second real commitment

--- lecture-code/copy_bad_method.py
+++ lecture-code/copy_bad_method.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+"""
+At the memory location of x, point y to the same spot.
+Nikki = Nikki-kara
+"""
+
+
 x = np.linspace(10, 12, num=3)
 y = x       # DON'T do this to copy an array
 
--- lecture-code/copy_demo.py
+++ lecture-code/copy_demo.py
@@ -1,7 +1,14 @@
 import numpy as np
 
+'''
+x = blah, we want another one but not the same one with a different name.
+
+'''
+
 x = np.linspace(10, 12, num=3)
 y = np.copy(x)
+
+y[0] = 0.0
 
 print(x)
 print(y)
